b2d4048fa9c15a98f0e844819beb28946ceac96f
magi2004 <magiabalans22it@srishakthi.ac.in>
2025-07-12 12:25:53 +0530

    new : added morgan

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
 const dotenv = require('dotenv');
 
 // Load environment variables
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
